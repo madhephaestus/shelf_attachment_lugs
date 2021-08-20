4e5519c8042cc3a836cef24776233201f3113830
Kevin Harrington <kevin.harrington@halodi.com>
2021-08-20 13:50:24 -0400

    making the shelf holder

--- lugs.groovy
+++ lugs.groovy
@@ -1,1 +1,23 @@
 //Your code here
+CSG myCylinder = new Cylinder(30.56/2.0, // Radius at the bottom
+                      		28.71/2.0, // Radius at the top
+                      		44, // Height
+                      		(int)30 //resolution
+                      		).toCSG()//convert to CSG to display  
+CSG core =new Cylinder(25.6/2.0,44).toCSG() // a one line Cylinder
+
+CSG rib =new Cylinder(28.71/2.0,1).toCSG()
+			.difference(
+				new Cylinder(24.6/2.0,1).toCSG()) // a one line Cylinder
+			.movez(myCylinder.getMaxZ()-13.3)
+
+CSG spacing = new Cube(2,30.56,44).toCSG()
+				.toZMin()
+
+def corebit=myCylinder
+			.difference(core)				
+			.union(rib)
+			.difference(spacing)
+
+
+return [corebit]
